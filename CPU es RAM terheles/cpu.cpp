4cf0836e091d5f1d5e3e9b620f3045b3ea49956b
kistamara88-ctrl <kis.tamara88@gmail.com>
2026-02-17 11:56:25 +0100

    Add files via upload

--- CPU es RAM terheles/cpu.cpp
+++ CPU es RAM terheles/cpu.cpp
@@ -4,17 +4,11 @@ using namespace std;
 
 void workerFunction() {
     while (true) {
-        
-    }
-}
-
+    }}
 int main() {
     const int numThreads = thread::hardware_concurrency();
-
     for (int i = 0; i < numThreads; ++i) {
-        thread(workerFunction).detach();
-    }
-
+        thread(workerFunction).detach(); }
     this_thread::sleep_for(chrono::seconds(10));
 
     return 0;
